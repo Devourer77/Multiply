9a915ed830d9601a1452298f471b175c442f50a1
devourer77 <devourer77@gmail.com>
2019-10-13 16:10:52 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    var firstS = first.split('').reverse();
+    var secondS = second.split('').reverse();
+
+    var Result = [];
+
+    for (var i = 0; i < firstS.length; i++) {
+        for (var a = 0; a < secondS.length; a++) {
+            var m = firstS[i] * secondS[a];
+            Result[i + a] = (Result[i + a]) ? Result[i + a] + m : m;
+        }
+    }
+
+    for (var i = 0; i < Result.length; i++) {
+        var numb = Result[i] % 10;
+        var namA = Math.floor(Result[i] / 10);
+        Result[i] = numb;
+
+        if (Result[i + 1])
+            Result[i + 1] += namA;
+        else if (namA != 0)
+            Result[i + 1] = namA;
+    }
+
+
+    return Result.reverse().join('');
+};
